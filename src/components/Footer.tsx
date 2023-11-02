4f8133055fb346a98c638dc17010a3ae49fece9e
David Riley <Dril112191@gmail.com>
2023-11-02 12:45:36 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = ( ) => {
 
     return(
         <footer className=" py-10 mt-32 border-t">
-            <div className="flex flex-col items-center sm:flex-row pl-10 sm:justify-between lg:w-[80%] lg:mx-auto">
+            <div className="flex flex-col items-center sm:flex-row sm:justify-between lg:w-[85%] lg:mx-auto">
                 <p className="flex flex-col">Built and Designed by<span className="text-center sm:text-left">David Riley</span> </p>
                 <a className="my-8 sm:my-0 border py-3 px-12 btn-hover font-semibold text-lg" href="https://docs.google.com/document/d/1K-TI-o4oTf1njuind_5i-n4trzjysyPxSXZN1UVnSz4/edit?usp=sharing" target="_blank">Resume</a>
                 <div className="flex gap-6 sm:pr-10 ">
